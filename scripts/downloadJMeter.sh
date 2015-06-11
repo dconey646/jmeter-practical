d4b91b27ac213d3d7fccff5da8900c5a590d404f
Damien Coney <d.coney@kainos.com>
2015-06-11 10:07:39 +0100

    adding scripts to pull the jmeter 2.13 files.

---
+++ scripts/downloadJMeter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+JMETER_DIR="$1"
+JMETER_URL="http://www.mirrorservice.org/sites/ftp.apache.org/jmeter/binaries/apache-jmeter-2.13.tgz"
+
+white=`tput setaf 7`
+reset=`tput sgr0`
+
+function install_jmeter() {
+    echo "${white}Downloading Apache JMeter from ${JMETER_URL}${reset}"
+    curl -o jmeter.tgz ${JMETER_URL}
+    tar -xzf ./jmeter.tgz
+    rm -rf ./jmeter.tgz
+    mv apache-jmeter-* ${JMETER_DIR}
+    echo "${white}Apache JMeter has been installed under ${JMETER_DIR}${reset}"
+    echo "jmeter.save.saveservice.output_format=xml" >> ${JMETER_DIR}/bin/jmeter.properties
+    echo "CookieManager.delete_null_cookies=false
+          CookieManager.save.cookies=true
+          CookieManager.check.cookies=false" >> ${JMETER_DIR}/bin/user.properties
+}
+
+if [[ -z "$1" || "$1" == "-h" ]]
+then
+    echo "Usage: downloadJMeter.sh installation_path"
+    exit 1
+fi
+
+if [ ! -e ${JMETER_DIR} ]
+then
+    install_jmeter
+else
+    echo "${white}Apache JMeter is already installed at ${JMETER_DIR}${reset}"
+fi
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir jMeter/
+echo "Downloading JMeter 2.13"
+./downloadJMeter.sh ../jmeter
+echo "Download complete :D"
